5af87b6733fec4d7ac70e0076c14aac9a7e91bc8
traper <52043756+traperwaze@users.noreply.github.com>
2022-02-10 16:34:40 +0700

    small refactor

--- main.go
+++ main.go
@@ -11,9 +11,6 @@ var counter int
 func main() {
 	InitConfig()
 
-	// this line makes requests without waiting first interval
-	sendRequests()
-
 	interval, err := time.ParseDuration(Config.Interval)
 	if err != nil {
 		panic("Error parsing interval. Change it in config.json. e.g 1m, 1h10m, 10s, etc.")
@@ -21,26 +18,33 @@ func main() {
 
 	c := time.Tick(interval)
 
-	for range c {
-		counter++
+	go func() {
+		// this line makes requests without waiting first interval
 		sendRequests()
-	}
+
+		for range c {
+			counter++
+			sendRequests()
+		}
+	}()
+
+	select {}
 }
 
-func sendRequest(url string) {
-	fmt.Printf("[%d] Sending request to %s\n", counter, url)
+func sendRequest(url string, count int) {
+	fmt.Printf("[%d] Sending request to %s\n", count, url)
 
 	res, err := http.Head(url)
 	if err != nil {
-		fmt.Printf("[%d] Error: %s\n", counter, err)
+		fmt.Printf("[%d] Error: %s\n", count, err)
 		return
 	}
 
-	fmt.Printf("[%d] Sent to %s. status:%d\n", counter, url, res.StatusCode)
+	fmt.Printf("[%d] Sent to %s. status:%d\n", count, url, res.StatusCode)
 }
 
 func sendRequests() {
 	for _, url := range Config.Urls {
-		go sendRequest(url)
+		go sendRequest(url, counter)
 	}
 }
