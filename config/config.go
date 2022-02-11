2678df462f075563f9bf59e18820e63e3b190595
traper <52043756+traperwaze@users.noreply.github.com>
2022-02-11 23:25:35 +0700

    enclosing d33f948344ebda240adbae1d5c856c24f0464a91

---
+++ config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+
+	"github.com/tkanos/gonfig"
+)
+
+type configuration struct {
+	Interval string
+	Urls     []string
+}
+
+var Config *configuration
+var filename string = "config.json"
+
+func InitConfig() {
+	fmt.Println("[config path]", getFileName())
+
+	Config = &configuration{}
+	
+	err := gonfig.GetConf(getFileName(), Config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if urlsEmpty() {
+		log.Fatal("[config error] urls is empty")
+	}
+}
+
+func getFileName() string {
+	filepath := path.Join(wd(), filename)
+
+	return filepath
+}
+
+func urlsEmpty() bool {
+	return len(Config.Urls) == 0
+}
+
+func wd() string {
+	p, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return p
+}
